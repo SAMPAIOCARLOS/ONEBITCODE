e9e3e36609b4dc1d81b1ec2c90312e5cfef02ab6
SAMPAIOCARLOS <sampaiocarlos957@gmail.com>
2023-08-09 16:18:13 -0300

    novos projetos

---
+++ EX011/index.js
@@ -0,0 +1,23 @@
+let primeiroVeiculo = window.prompt("Digite o nome do primeiro Veículo")
+
+let segundoVeiculo = window.prompt("Digite o nome do segundo veículo")
+
+let velocidadeP = Number(window.prompt("Informe a velocidade do primeiro veículo"))
+
+let velocidadeS = Number(window.prompt("Informe a velocidade do segundo veiculo"))
+
+
+if(velocidadeP === velocidadeS){
+    window.alert(`A VELOCIDADE DOS DOIS VEÍCULOS SÃO IGUAIS `)
+    
+    Document.window(`A VELOCIDADE DO ${primeiroVeiculo} e do ${segundoVeiculo} SÃO A MESMA!`)
+    
+}if(velocidadeP > velocidadeS){
+    window.alert(`O ${primeiroVeiculo} é mais rápido que o ${segundoVeiculo}`)
+
+    document.write(`${primeiroVeiculo} CAMPEÃO`)
+}else{
+    window.alert(`O ${segundoVeiculo} é mais rápido que o ${primeiroVeiculo}`)
+
+    document.write(`O ${segundoVeiculo} é CAMPEÃO`)
+}
---
+++ EX012/jogo.js
@@ -0,0 +1,26 @@
+let nomePersonagem1 = window.prompt("Nome do primeiro personagem")
+
+let poderDeAtaquePersonagem1 = Number(window.prompt(`Informe o poder de ataque do seu primeiro heroi: ${nomePersonagem1}`))
+
+let pontosDeVidaPersonagem1 = Number(window.prompt(`Ìnforme a quantidade de vida do seu heroi: ${nomePersonagem1}`))
+
+let poderDeDefesa = Number(window.prompt(`Informe o poder de defesa do seu heroi: ${nomePersonagem1}`))
+
+let escudo = window.confirm(`Seu heroi possui um escudo`)
+
+
+let nomePersonagem2 = window.prompt("Nome do segundo personagem")
+
+let poderDeAtaquePersonagem2 = Number(window.prompt(`Informe o poder de ataque do seu segundo heroi: ${nomePersonagem2}`))
+
+let pontosDeVidaPersonagem2 = Number(window.prompt(`Ìnforme a quantidade de vida do seu heroi: ${nomePersonagem2}`))
+
+let poderDeDefesa2 = Number(window.prompt(`Informe o poder de defesa do seu heroi: ${nomePersonagem2}`))
+
+let escudo2 = window.confirm(`Seu heroi possui um escudo`)
+
+if(poderDeAtaquePersonagem1 > pontosDeVidaPersonagem2 || escudo2 === false){
+    alert(`A vida do ${nomePersonagem2} era de ${pontosDeVidaPersonagem2} e agora é ${pontosDeVidaPersonagem2 - poderDeAtaquePersonagem1}`)
+}if(escudo2 === true){
+    alert(`A vida de ${nomePersonagem2} era de ${pontosDeVidaPersonagem2} e agora é de ${pontosDeVidaPersonagem2 - poderDeAtaquePersonagem1 + poderDeDefesa2}`)
+}
