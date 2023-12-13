e8b02e861d272c09087e4ac4c66eb47ac0d9b57c
SAMPAIOCARLOS <sampaiocarlos957@gmail.com>
2023-12-13 16:11:20 -0300

    teste

---
+++ EX036/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class OlaMundo extends React.Component {
+  render() {
+    return (
+    <div>
+      <h1>Olá, Mundo!</h1>
+    </div>
+    );
+  }
+}
+
+export default OlaMundo;
